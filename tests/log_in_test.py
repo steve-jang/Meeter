96aa20a62f9013b536b23e288898ad03ea8955b0
Steve Jang <stevejang@SJs-MacBook-Pro.local>
2021-01-21 17:03:52 +1100

    Refine tests for log_in()

--- tests/log_in_test.py
+++ tests/log_in_test.py
@@ -7,7 +7,7 @@ import pytest
 from data import data
 from auth import log_in
 from error import AuthError
-from helpers import BOT_PASSWORD
+from helpers import BOT_PASSWORD, expect_error
 
 
 def test_bad_username():
@@ -15,33 +15,31 @@ def test_bad_username():
     Test a non existent username
     """
     # No users currently exist, so any username is invalid
-    with pytest.raises(AuthError):
-        log_in("badusername", "...")
+    expect_error(log_in, AuthError, "badusername", "...")
 
 
-def test_bad_password(bot):
+def test_bad_password(logged_out_bot):
     """
     Test an incorrect password
     """
-    with pytest.raises(AuthError):
-        log_in(bot.username, BOT_PASSWORD + "bad")
+    expect_error(log_in, AuthError,
+                 logged_out_bot.username, BOT_PASSWORD + "bad")
 
 
-def test_success(bot):
+def test_success(logged_out_bot):
     """
     Test a successful login
     """
-    assert log_in(bot.username, BOT_PASSWORD) == None
+    assert log_in(logged_out_bot.username, BOT_PASSWORD) == None
 
     # Check if data was updated
-    assert data.users.get(bot.username) == bot
+    assert data.users.get(logged_out_bot.username) ==logged_out_bot
 
 
-def test_already_logged_in(bot):
+def test_already_logged_in(logged_out_bot):
     """
     Test logging in when already logged in.
     """
-    log_in(bot.username, BOT_PASSWORD)
+    log_in(logged_out_bot.username, BOT_PASSWORD)
 
-    with pytest.raises(AuthError):
-        log_in(bot.username, BOT_PASSWORD)
+    expect_error(log_in, AuthError, logged_out_bot.username, BOT_PASSWORD)
