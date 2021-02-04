6187f11dfdc8a9a5cfecfa939acc27695f8c9af6
Steve Jang <stevejang@SJs-MacBook-Pro.local>
2021-02-04 14:57:24 +1100

    Write tests for event_details()

---
+++ tests/event_details_test.py
@@ -0,0 +1,63 @@
+"""
+Tests for event_details()
+"""
+
+
+from datetime import date, datetime, timedelta
+from data import data
+from event_data import event_details
+from error import InputError, AuthError
+from auth import log_out
+from event_admin import edit_event_deadline, edit_event_length
+from helpers import expect_error
+
+
+def test_invalid_username():
+    """
+    Test a non-existent username.
+    """
+    expect_error(event_details, InputError, "aaa", 1)
+
+
+def test_invalid_event(bot):
+    """
+    Test a non-existent event.
+    """
+    expect_error(event_details, InputError, bot.username, 1)
+
+
+def test_not_member(event_member):
+    """
+    Test when the user is not a member of the event.
+    """
+    _, member, event_id = event_member
+    expect_error(event_details, InputError, member.username, event_id)
+
+
+def test_not_logged_in(event_member):
+    """
+    Test when the user is not logged in.
+    """
+    _, member, event_id = event_member
+    log_out(member.username)
+    expect_error(event_details, AuthError, member.username, event_id)
+
+
+def test_success_details(event_member):
+    """
+    Test a successful case of getting event details.
+    """
+    admin, member, event_id = event_member
+    edit_event_length(admin.username, 24, event_id)
+    edit_event_deadline(admin.username, date.today() + timedelta(days=25), event_id)
+    result = event_details(member.username, event_id)
+    event = data.events.get(event_id)
+    expected_result = {
+        "members": {member.username, admin.username},
+        "admin": admin.username,
+        "create_time": event.create_time,
+        "length": event.event_length,
+        "deadline": event.event_deadline,
+    }
+
+    assert result == expected_result
